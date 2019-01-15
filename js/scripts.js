03a7cf992c7c9ab57fb9e207a7b220b3bcf43558
bertassi <bertassi@users.noreply.github.com>
2019-01-15 19:31:13 -0300

    Character counting for the contact form message field.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,26 @@
-function myAlert() {
-    alert("Esta funcionalidade será implementada em breve.");
+function myAlert() 
+{
+  alert("Esta funcionalidade será implementada em breve.");
+}
+
+function submissionSuccess()
+{
+    alert("Sua mensagem foi enviada com sucesso! Entraremos em contato o mais breve possível.");
+}
+
+function charCountUpdate(textbox, counter) 
+{
+  var maxlimit = 100;
+  var lng = document.getElementById(textbox).value.length;
+  
+  // If too long...trim it!
+  if (document.getElementById(textbox).value.length > maxlimit) 
+  {        
+        document.getElementById(textbox).value = document.getElementById(textbox).value.substring(0, maxlimit);
   }
+  // Otherwise, update 'characters left' counter
+  else 
+  {        
+      document.getElementById(counter).innerHTML = lng + ' de ' + maxlimit + ' caracteres.';
+  }	
+}
